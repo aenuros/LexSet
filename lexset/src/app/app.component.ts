f153bed62e81f9da8131c51669d390f9359fd29e
Karen Burgos <kburgos@steelbridgeconsulting.com>
2019-09-20 13:29:27 -0400

    add initial design and logic

--- lexset/src/app/app.component.ts
+++ lexset/src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lexset';
+  myText = 'test';
+
+  public containsThisPhoneme(phoneme, song) {
+    console.log(phoneme);
+    // this is the entire line
+    let byNewLine = song.split("\n");
+    for (let i = 0; i <byNewLine.length;i++) {
+        let currentLine = byNewLine[i].split(" ");
+        for (let j=0;j < currentLine.length; j++) {
+            const word =  currentLine[j].toUpperCase();
+            if (dictionary[word] && dictionary[word]["arpabet"].includes(phoneme)) {
+                console.log(`Line: ${currentLine}`);
+                console.log(`${currentLine[j]}: ${dictionary[word]["arpabet"]}`);
+                console.log('********')
+            }
+        }
+    }
+}
+
+  public analyzeText() {
+    console.log(this.myText);
+  }
 }
