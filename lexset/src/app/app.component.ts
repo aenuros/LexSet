d2b9919adc5da9bc6b858827ddd86b6365159dc4
Karen Burgos <kburgos@steelbridgeconsulting.com>
2019-09-22 21:40:19 -0400

    Move dictionary to service

--- lexset/src/app/app.component.ts
+++ lexset/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DictionaryService } from './dictionary.service';
 
 @Component({
   selector: 'app-root',
@@ -8,22 +9,30 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'lexset';
   myText = 'test';
+  dictionary;
 
-  public containsThisPhoneme(phoneme, song) {
-    console.log(phoneme);
-    // this is the entire line
-    let byNewLine = song.split("\n");
-    for (let i = 0; i <byNewLine.length;i++) {
-        let currentLine = byNewLine[i].split(" ");
-        for (let j=0;j < currentLine.length; j++) {
-            const word =  currentLine[j].toUpperCase();
-            if (dictionary[word] && dictionary[word]["arpabet"].includes(phoneme)) {
-                console.log(`Line: ${currentLine}`);
-                console.log(`${currentLine[j]}: ${dictionary[word]["arpabet"]}`);
-                console.log('********')
-            }
-        }
-    }
+constructor(private dictionaryService: DictionaryService) {
+  this.dictionaryService.getAllWords().subscribe(resp => {
+    this.dictionary = resp;
+  });
+}
+
+
+public containsThisPhoneme(phoneme, song) {
+  console.log(phoneme);
+  // this is the entire line
+  const byNewLine = song.split('\n');
+  for (let i = 0; i < byNewLine.length; i++) {
+      const currentLine = byNewLine[i].split(' ');
+      for (let j = 0; j < currentLine.length; j++) {
+          const word =  currentLine[j].toUpperCase();
+          if (this.dictionary[word] && this.dictionary[word]['arpabet'].includes(phoneme)) {
+              console.log(`Line: ${currentLine}`);
+              console.log(`${currentLine[j]}: ${this.dictionary[word]['arpabet']}`);
+              console.log('********');
+          }
+      }
+  }
 }
 
   public analyzeText() {
