5e3259d7e7b528ca57ca27b0474e7287167ce514
Karen Burgos <kburgos@steelbridgeconsulting.com>
2019-12-05 17:20:19 -0500

    remove console logging and display results to page; add lexical set representation

--- lexset/src/app/app.component.ts
+++ lexset/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { DictionaryService } from './dictionary.service';
+import { FormControl } from '@angular/forms';
+import { DictionaryService } from './dictionary.service';;
 
 @Component({
   selector: 'app-root',
@@ -10,32 +11,65 @@ export class AppComponent {
   title = 'lexset';
   myText = 'test';
   dictionary;
+  line: string;
+  result: string;
+  resultsArray = [];
+  // resultObject = {};
+  // TODO - create interface for a result Object
 
-constructor(private dictionaryService: DictionaryService) {
-  this.dictionaryService.getAllWords().subscribe(resp => {
-    this.dictionary = resp;
-  });
-}
-
-
-public containsThisPhoneme(phoneme, song) {
-  console.log(phoneme);
-  // this is the entire line
-  const byNewLine = song.split('\n');
-  for (let i = 0; i < byNewLine.length; i++) {
-      const currentLine = byNewLine[i].split(' ');
-      for (let j = 0; j < currentLine.length; j++) {
-          const word =  currentLine[j].toUpperCase();
-          if (this.dictionary[word] && this.dictionary[word]['arpabet'].includes(phoneme)) {
-              console.log(`Line: ${currentLine}`);
-              console.log(`${currentLine[j]}: ${this.dictionary[word]['arpabet']}`);
-              console.log('********');
-          }
-      }
+  constructor(private dictionaryService: DictionaryService) {
+    this.dictionaryService.getAllWords().subscribe(resp => {
+      this.dictionary = resp;
+    });
   }
-}
 
-  public analyzeText() {
-    console.log(this.myText);
+
+  public containsThisPhoneme(phoneme, song) {
+
+    const byNewLine = song.split('\n');
+    for (let i = 0; i < byNewLine.length; i++) {
+        const currentLine = byNewLine[i].split(' ');
+
+        for (let j = 0; j < currentLine.length; j++) {
+            const word =  currentLine[j].toUpperCase();
+            if (this.dictionary[word] && this.dictionary[word]['arpabet'].includes(this.convertLexicalSetToPhoneticDictionary(phoneme))) {
+                const resultObj = {
+                  line: byNewLine[i],
+                  result: currentLine[j]
+                };
+                this.resultsArray.push(resultObj);
+            }
+        }
+    }
+  }
+
+  public convertLexicalSetToPhoneticDictionary(lexicalSet: string): string {
+    switch (lexicalSet) {
+      case 'KIT': return 'IH';
+      case 'DRESS': return 'EH';
+      case 'TRAP': return 'AE';
+      case 'LOT': return 'AA';
+      case 'STRUT': return 'AH';
+      case 'FOOT': return 'UH';
+      case 'BATH': return 'AE';
+      case 'CLOTH': return 'AO';
+      case 'NURSE': return 'ER';
+      case 'FLEECE': return 'IY';
+      case 'FACE': return 'EY';
+      case 'PALM': return 'AA';
+      case 'THOUGHT': return 'AO';
+      case 'GOAT': return 'OW';
+      case 'GOOSE': return 'UW';
+      case 'PRICE': return 'AY';
+      case 'CHOICE': return 'OY';
+      case 'MOUTH': return 'AW';
+      // TODO - the following require more
+      // case 'NEAR': return ;
+      // case 'SQUARE': return;
+      // case 'START': return;
+      // case 'NORTH': return;
+      // case 'FORCE': return;
+      // case 'CURE': return;
+    }
   }
 }
